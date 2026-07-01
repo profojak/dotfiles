6680fb9577103c2b67f02d38e02493f6accfac81
Jakub Profota <245464941+profojak@users.noreply.github.com>
2026-07-01 17:02:37 +0200

    💎 Nix: Add Pi to profile flake

--- flake.nix
+++ flake.nix
@@ -3,12 +3,14 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    llm-agents.url = "github:numtide/llm-agents.nix";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, llm-agents }:
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
+      agents = llm-agents.packages.${system};
     in
     {
       packages.${system}.default = pkgs.buildEnv {
@@ -16,6 +18,8 @@
         paths = builtins.attrValues {
           inherit (pkgs)
           helix;
+          inherit (agents)
+          pi;
         };
       };
     };
